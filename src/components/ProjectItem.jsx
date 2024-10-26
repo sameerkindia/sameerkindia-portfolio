7ab3a226e5725915316e5ec38147199a4bd1644c
sameerkindia <sameerkhanaa5@gmail.com>
2024-10-26 08:07:12 +0530

    project item ui updated

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -6,11 +6,13 @@ function ProjectItem({ title, id, tecnology, url, imgSrc }) {
   return (
     <li className={styles.project}>
       <img src={imgSrc} alt={title} />
-      <p className={styles.subHeading}>{title}</p>
-      <p className={styles.tecnology}>{tecnology.join(",")}</p>
-      <a className={styles.link} href={url} target="_blank">
-        Live Site
-      </a>
+      <div className={styles.info}>
+        <p className={styles.subHeading}>{title}</p>
+        <p className={styles.tecnology}>{tecnology.join(",")}</p>
+        <a className={styles.link} href={url} target="_blank">
+          Live Site
+        </a>
+      </div>
     </li>
   );
 }
